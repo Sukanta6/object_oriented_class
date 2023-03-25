4adf78afb7b4b11f3311d39e76a91392929514a1
sukanta <sukanta6@gmail.com>
2023-03-25 20:31:27 +0530

    modified

--- simple_class_method.py
+++ simple_class_method.py
@@ -1,7 +1,22 @@
 
 # Simple Class with instances
 class Employee:
-    #pass
+    pass
+
+emp_1= Employee()
+emp_2= Employee()
+
+emp_1.first = 'sukanta'
+emp_1.last = 'biswas'
+
+emp_2.first = 'John'
+emp_2.last = 'Cook'
+
+print(emp_1.first, emp_1.last, emp_1.first+'.'+emp_1.last+'@gmail.com')
+print(emp_2.first, emp_2.last, emp_2.first+'.'+emp_2.last+'@gmail.com')
+
+
+class employee_evo():
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -12,11 +27,8 @@ class Employee:
     def fullname(self):
         return '{} {} {}'.format(self.first, self.last, self.email)
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
-
-#emp_1 = Employee()
-#print(emp_1.first, emp_1.last, emp_1.email)
+emp_1 = employee_evo('Sukanta', 'Biswas', 50000)
+emp_2 = employee_evo('John', 'Cook', 60000)
 
 print(emp_1.fullname())
-print(Employee.fullname(emp_2))
+print(employee_evo.fullname(emp_2))
